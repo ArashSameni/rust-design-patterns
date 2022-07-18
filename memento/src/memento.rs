765a657d9c91297bed58f09b5204df46a073b517
Arash Sameni <arashsameniy@gmail.com>
2022-07-19 03:08:09 +0430

    Memento design pattern

---
+++ memento/src/memento.rs
@@ -0,0 +1,60 @@
+pub struct Originator {
+    state: String,
+}
+
+impl Originator {
+    pub fn new() -> Self {
+        Originator {
+            state: String::new(),
+        }
+    }
+
+    pub fn state(&self) -> &str {
+        &self.state
+    }
+
+    pub fn set_state(&mut self, state: &str) {
+        self.state = state.to_owned();
+    }
+
+    pub fn save(&self) -> Memento {
+        Memento {
+            state: self.state.clone(),
+        }
+    }
+
+    pub fn restore(&mut self, memento: Memento) {
+        self.state = memento.state;
+    }
+}
+
+pub struct Memento {
+    pub state: String,
+}
+
+pub struct Caretaker {
+    mementos: Vec<Memento>,
+}
+
+impl Caretaker {
+    pub fn new() -> Self {
+        Caretaker { mementos: vec![] }
+    }
+
+    pub fn backup(&mut self, memento: Memento) {
+        self.mementos.push(memento);
+    }
+
+    pub fn undo(&mut self) -> Result<Memento, &'static str> {
+        match self.mementos.pop() {
+            Some(state) => Ok(state),
+            None => Err("nothing is saved"),
+        }
+    }
+
+    pub fn show_history(&self) {
+        for memento in self.mementos.iter().rev() {
+            println!("{}", memento.state);
+        }
+    }
+}
